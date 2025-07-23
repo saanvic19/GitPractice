03abae734070225b21a66ace96df7d8ea2451b7e
Saanvi <chsaanvi@gmail.com>
2025-07-23 13:10:57 -0500

    added more text

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("My favorite color is pink!")
+            Text("My favorite color is also green!")
         }
         .padding()
     }
